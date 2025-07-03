4616d08b3707913c8eea934d47283e5c4f317983
Sanskar-Rijal <rijalsanskar@gmail.com>
2025-07-03 22:19:40 +0545

    using our own port to fetch data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,51 @@
-import { DateCounter } from "./DateCounter";
+//import { DateCounter } from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+const initialstate = {
+  questions: [],
+  //we can be loading,error,ready,active,finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error(`Action Unknown`);
+  }
+}
 
 export default function App() {
-  return <div>{/* <DateCounter /> for practice of useReducer hook*/}</div>;
+  //using useReducer hook for displaying question
+  const [state, dispatch] = useReducer(reducer, initialstate);
+
+  useEffect(function () {
+    async function getQsn() {
+      try {
+        const response = await fetch(`http://localhost:8000/questions`);
+
+        const data = await response.json();
+        dispatch({
+          type: "dataReceived",
+          payload: data,
+        });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    getQsn();
+  }, []);
+
+  return (
+    <div className="app">
+      {/* <DateCounter /> for practice of useReducer hook*/}
+      <Header />
+      <Main />
+    </div>
+  );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main({ children }) {
+  return (
+    <main className="main">
+      <p>1/15</p>
+      <p>Question</p>
+    </main>
+  );
+}
